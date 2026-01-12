e1aa2f2b802e3f9c3379daf30f73b19f2ce9921d
raska26 <radityasandika51@gmail.com>
2026-01-12 23:07:53 +0700

    Menambahkan endpoint health check API

--- BACKEND/SRC/server.js
+++ BACKEND/SRC/server.js
@@ -4,6 +4,10 @@ import { ENV } from "./config/env.js";
 const app = express();
 const PORT = ENV.PORT || 5001;
 
+app.get("/api/health", (req, res) => {
+  res.status(200).json({ success: true });
+});
+
 app.listen(PORT, () => {
   console.log("Server is running on PORT:", PORT);
 });
