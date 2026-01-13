ccc71db81e71afde66c1394c1b86d65fe2d227c8
raska26 <radityasandika51@gmail.com>
2026-01-13 18:11:03 +0700

    Menambahkan tabel favorites untuk menyimpan daftar favorit user

--- BACKEND/SRC/db/schema.js
+++ BACKEND/SRC/db/schema.js
@@ -1,1 +1,7 @@
 import { pgTable, serial, text, timestamp, integer } from "drizzle-orm/pg-core";
+
+export const favorites = pgTable("favorites", {
+  id: serial("id").primaryKey(),
+  userId: integer("user_id").notNull(),
+  
+});
