e9ada9c2f14f9848aac742b6fb5faaeac446b94b
raska26 <radityasandika51@gmail.com>
2026-01-13 18:14:36 +0700

    Menambahkan kolom recipeId dan title pada tabel favorites

--- BACKEND/SRC/db/schema.js
+++ BACKEND/SRC/db/schema.js
@@ -3,5 +3,7 @@ import { pgTable, serial, text, timestamp, integer } from "drizzle-orm/pg-core";
 export const favorites = pgTable("favorites", {
   id: serial("id").primaryKey(),
   userId: integer("user_id").notNull(),
+  recipeId: integer("recipe_id").notNull(),
+  title: text("title").notNull(),
   
 });
