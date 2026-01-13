8d322d196eea22d40606801f82d11eb085fcbe67
raska26 <radityasandika51@gmail.com>
2026-01-14 04:35:13 +0700

    benerin eror

--- BACKEND/SRC/config/db.js
+++ BACKEND/SRC/config/db.js
@@ -1,4 +1,10 @@
 import { drizzle } from "drizzle-orm/neon-http";
 import { neon } from "@neondatabase/serverless";
-import { env } from "./env.js";
+import { ENV } from "./env.js"; 
 import * as schema from "../db/schema.js";
+
+const client = neon({
+  url: ENV.DATABASE_URL,
+});
+
+export const db = drizzle(client, { schema });
--- BACKEND/SRC/db/schema.js
+++ BACKEND/SRC/db/schema.js
@@ -1,11 +1,11 @@
 import { pgTable, serial, text, timestamp, integer } from "drizzle-orm/pg-core";
 
-export const favorites = pgTable("favorites", {
+export const favoritesTable = pgTable("favorites", {
   id: serial("id").primaryKey(),
-  userId: integer("user_id").notNull(),
+  userId: text("user_id").notNull(),
   recipeId: integer("recipe_id").notNull(),
   title: text("title").notNull(),
-  Image: text("image"),
+  image: text("image"),
   cookTime: text("cook_time"),
   servings: text("servings"),
   createdAt: timestamp("created_at").defaultNow(),
