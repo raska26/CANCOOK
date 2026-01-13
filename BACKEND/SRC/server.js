b5ce6128a8601b8006ab742af05c5d4ade8e1277
raska26 <radityasandika51@gmail.com>
2026-01-14 03:58:51 +0700

    tambah POST favorites untuk favorit pengguna

--- BACKEND/SRC/server.js
+++ BACKEND/SRC/server.js
@@ -9,6 +9,14 @@ app.get("/api/health", (req, res) => {
 });
 
 app.post("/api/favorites", async (req, res) => {
+   try {
+    const { userId, recipeId, title, image, cookTime, servings } = req.body;
+
+    if (!userId || !recipeId || !title) {
+      return res.status(400).json({ error: "Missing required fields" });
+    }
+   
+
 
 app.listen(PORT, () => {
   console.log("Server is running on PORT:", PORT);
