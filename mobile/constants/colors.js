79fe1f38c2dd61415f198e60364e73d20b6e1785
damanik27 <wilsondamanik27@gmail.com>
2026-01-15 21:13:59 +0700

    buat oceanTheme

--- mobile/constants/colors.js
+++ mobile/constants/colors.js
@@ -33,5 +33,16 @@ const purpleTheme = {
   shadow: "#000000",
 };
 
+const oceanTheme = {
+  primary: "#0277BD",
+  background: "#E1F5FE",
+  text: "#01579B",
+  border: "#B3E5FC",
+  white: "#FFFFFF",
+  textLight: "#4FC3F7",
+  card: "#FFFFFF",
+  shadow: "#000000",
+};
+
 
 export const COLORS = THEMES.purple;
