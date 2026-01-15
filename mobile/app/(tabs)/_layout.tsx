5f4f566ac20e5b92ca32e2edf5a652c3b189508e
damanik27 <wilsondamanik27@gmail.com>
2026-01-16 06:19:18 +0700

    memperbahurin layout

--- mobile/app/(tabs)/_layout.tsx
+++ mobile/app/(tabs)/_layout.tsx
@@ -1,5 +1,9 @@
 import { Stack } from "expo-router";
+import { SafeAreaView } from "react-native-safe-area-context/lib/typescript/src/SafeAreaView";
 
 export default function RootLayout() {
-    return <Stack/>;
+    return 
+    <SafeAreaView>
+        <Stack screenOptions={{ headerShown: false}}/>
+    </SafeAreaView>
 }
