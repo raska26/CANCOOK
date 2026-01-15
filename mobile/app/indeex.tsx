70931e2d9303fde317e414080c6613aaa353aa49
damanik27 <wilsondamanik27@gmail.com>
2026-01-15 19:09:41 +0700

    setup expo router & react hooks

---
+++ mobile/app/indeex.tsx
@@ -0,0 +1,3 @@
+import { View, Text, ScrollView, TouchableOpacity, FlatList, RefreshControl } from "react-native";
+import { useEffect, useState } from "react";
+import { useRouter } from "expo-router";
