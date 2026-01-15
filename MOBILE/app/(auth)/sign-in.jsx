d37597f81e5b8f77488d07fa6a372992d12db219
raska26 <radityasandika51@gmail.com>
2026-01-15 23:39:31 +0700

    menambahkan SignInScreen dan navigasi menggunakan useRouter

--- MOBILE/app/(auth)/sign-in.jsx
+++ MOBILE/app/(auth)/sign-in.jsx
@@ -1,6 +1,9 @@
 import { View, Text } from 'react-native'
 import React from 'react'
 
+const SignInScreen = () => {
+  const router = useRouter();
+
 export default function sign-in() {
   return (
     <View>
