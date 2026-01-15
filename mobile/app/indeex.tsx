8f8eb4b82a3016b33f2baca936fd3ce67c6c9abc
damanik27 <wilsondamanik27@gmail.com>
2026-01-15 19:10:50 +0700

    integrate MealAPI service

--- mobile/app/indeex.tsx
+++ mobile/app/indeex.tsx
@@ -1,3 +1,5 @@
 import { View, Text, ScrollView, TouchableOpacity, FlatList, RefreshControl } from "react-native";
 import { useEffect, useState } from "react";
 import { useRouter } from "expo-router";
+import { MealAPI } from "../../services/mealAPI";
+const sleep = (ms) => new Promise(...)
