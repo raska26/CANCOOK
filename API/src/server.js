ab3b7657e2a515ff8509f5ec790e5cd129e366bf
damanik27 <wilsondamanik27@gmail.com>
2026-01-15 18:46:10 +0700

    tambah import

--- API/src/server.js
+++ API/src/server.js
@@ -2,6 +2,8 @@ import express from "express";
 import { ENV } from "./config/env.js";
 import { db } from "./config/db.js";
 import { favoritesTable } from "./db/schema.js";
+import { and, eq } from "drizzle-orm";
+import job from "./config/cron.js";
 
 
 
