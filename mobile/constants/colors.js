c159a7d3159d4b2a69780f26e9cfd62abd344058
damanik27 <wilsondamanik27@gmail.com>
2026-01-15 21:10:10 +0700

    buat colors

---
+++ mobile/constants/colors.js
@@ -0,0 +1,14 @@
+const coffeeTheme = {
+  primary: "#8B593E",
+  background: "#FFF8F3",
+  text: "#4A3428",
+  border: "#E5D3B7",
+  white: "#FFFFFF",
+  textLight: "#9A8478",
+  card: "#FFFFFF",
+  shadow: "#000000",
+};
+
+
+
+export const COLORS = THEMES.purple;
