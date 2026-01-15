6ededc44c9f2b776fcbd4e95fab448648698e1fd
damanik27 <wilsondamanik27@gmail.com>
2026-01-15 19:14:35 +0700

    import home

--- mobile/app/indeex.tsx
+++ mobile/app/indeex.tsx
@@ -2,4 +2,7 @@ import { View, Text, ScrollView, TouchableOpacity, FlatList, RefreshControl } fr
 import { useEffect, useState } from "react";
 import { useRouter } from "expo-router";
 import { MealAPI } from "../../services/mealAPI";
+import { homeStyles } from "../../assets/styles/home.styles";
+import { Image } from "expo-image";
+import { COLORS } from "../../constants/co
 const sleep = (ms) => new Promise(...)
